d550fee97ac941fb3896cb7e23756ea5d2cc896d
Devere-Anthony <devereaweaver@icloud.com>
2024-01-04 10:26:15 -0500

    just started working on collecting numbers for the Hotel Manzana application.

--- Tables and Persistence/Hotel Manzana/Hotel Manzana/View Controllers/AddRegistrationTableViewController.swift
+++ Tables and Persistence/Hotel Manzana/Hotel Manzana/View Controllers/AddRegistrationTableViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 class AddRegistrationTableViewController: UITableViewController {
     
 //==============================================================================
+// MARK: Class Properties
+//==============================================================================
+    // Store the index path  for the two date picker objects.
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+
+    // Initialize the date pickers to be hidden from the user. Change the
+    // the visibility state as the user interacts with the elements in the
+    // scene (the body of the computed variable changes its state).
+    var isCheckInDatePickerVisible: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerVisible
+        }
+    }
+    
+    var isCheckOutDatePickerVisible: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerVisible
+        }
+    }
+    
+    // Store the index path for check-in and check-out rows.
+    let checkInDateLabelCellIndexPath = IndexPath(row: 0, section: 1)
+    let checkOutDateLabelCellIndexPath = IndexPath(row: 2, section: 1)
+   
+//==============================================================================
 // MARK: Interface Builder Outlets
 //==============================================================================
     @IBOutlet var firstNameTextField: UITextField!
@@ -51,7 +77,56 @@ class AddRegistrationTableViewController: UITableViewController {
         checkOutDateLabel.text = checkOutDatePicker.date.formatted(date: .abbreviated,
                                                                    time: .omitted)
     }
-
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        /* Adjust the height of the date picker cells based on which date picker was
+         * selected and their current visibility. If the selected date picker is hidden
+         * then collapse their row heights. Keep all the other remaing rows at a default
+         * row height to present to the user. */
+        switch indexPath {
+        case checkInDatePickerCellIndexPath where isCheckInDatePickerVisible == false:
+            return 0
+        case checkOutDatePickerCellIndexPath where isCheckOutDatePickerVisible == false:
+            return 0
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        /* Estimate the row size for the date pickers. This will be needed to set the amount of space they'll
+         * need on the scene when they become visible after user interaction. */
+        switch indexPath {
+        case checkInDatePickerCellIndexPath: return 190
+        case checkOutDatePickerCellIndexPath: return 190
+        default: return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        /* Toggle the visibility of the check-in and check-out date pickers. */
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath == checkInDateLabelCellIndexPath && isCheckOutDatePickerVisible == false {
+            // Check-in selected & Check-out hidden => Expand check-in
+            isCheckInDatePickerVisible.toggle()
+        } else if indexPath == checkOutDateLabelCellIndexPath && isCheckInDatePickerVisible == false{
+            // Check-out select & Check-in hidden => Expand check-out
+            isCheckOutDatePickerVisible.toggle()
+        } else if indexPath == checkInDateLabelCellIndexPath || indexPath == checkOutDateLabelCellIndexPath {
+            // Either one selected => Expand the selected and hide the other one
+            isCheckInDatePickerVisible.toggle()
+            isCheckOutDatePickerVisible.toggle()
+        } else {
+            return
+        }
+        
+        // Instruct the table view to update itself and recalculate row heights.
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    
 //==============================================================================
 // MARK: Interface Builder Actions
 //==============================================================================
@@ -76,5 +151,4 @@ class AddRegistrationTableViewController: UITableViewController {
          * user updates the dates in the DatePickers. */
         updateDateViews()
     }
-    
 }
